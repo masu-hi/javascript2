5639e028433abf71a3ce1d9c8619ce568e4c1a0d
masu-hi <hikari09.masu@gmail.com>
2026-05-27 12:43:20 +0900

    授業の内容済、人気投票途中
    人気投票にアニメーションを取り入れている途中

---
+++ 0527/15_vote_new/src/main.js
@@ -0,0 +1,122 @@
+import { animate } from 'motion';
+
+import {
+  vote,
+  getRates,
+  getWinner
+} from './candidates.js';
+
+/* バー更新 */
+
+const animateBars = () => {
+
+  getRates().forEach(({ id, rate }) => {
+
+    const bar =
+      document.querySelector(
+        `[data-id="${id}"] .bar`
+      );
+
+    animate(
+      bar,
+      {
+        width: `${rate}%`
+      },
+      {
+        duration: 0.5,
+        easing: 'ease-out'
+      }
+    );
+
+  });
+
+};
+
+/* No.1表示 */
+
+const updateWinner = () => {
+
+  const winnerId = getWinner();
+
+  document.querySelectorAll('.card')
+    .forEach((card) => {
+
+      card.classList.remove('winner');
+
+      if (
+        Number(card.dataset.id) === winnerId
+      ) {
+
+        card.classList.add('winner');
+
+      }
+
+    });
+
+};
+
+/* ボタンクリック */
+
+document
+  .querySelectorAll('.card')
+  .forEach((card) => {
+
+    const id =
+      Number(card.dataset.id);
+
+    const btn =
+      card.querySelector('.vote-btn');
+
+    btn.addEventListener('click', () => {
+
+      /* 投票 */
+
+      vote(id);
+
+      /* ボタン */
+
+      animate(
+        btn,
+        {
+          scale: [1, 0.9, 1.2, 1],
+          rotate: [0, -5, 5, 0]
+        },
+        {
+          duration: 0.5
+        }
+      );
+
+      /* カード */
+
+      animate(
+        card,
+        {
+          y: [0, -15, 0]
+        },
+        {
+          duration: 0.4
+        }
+      );
+
+      /* 票数 */
+
+      const voteText =
+        card.querySelector('.votes');
+
+      animate(
+        voteText,
+        {
+          scale: [1, 1.5, 1]
+        },
+        {
+          duration: 0.4
+        }
+      );
+
+      animateBars();
+
+      updateWinner();
+
+    });
+
+  });
